8dc8ef416ebbbdb10be707e1cd9e42f15decb877
murray <murray.watts@lastyard.com>
2023-04-02 19:03:55 +0800

    Handle exit errors

--- app/main.go
+++ app/main.go
@@ -4,13 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	// Uncomment this block to pass the first stage!
-	// "os"
-	// "os/exec"
 )
 
-// https://stackoverflow.com/questions/48253268/print-the-stdout-from-exec-command-in-real-time-in-go
-// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
@@ -18,9 +13,13 @@ func main() {
 	cmd := exec.Command(command, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
 	err := cmd.Run()
 	if err != nil {
-		fmt.Printf("Err: %v", err)
-		os.Exit(1)
+		if exitError, ok := err.(*exec.ExitError); ok {
+			fmt.Println("Exit code:", exitError.ExitCode())
+			os.Exit(exitError.ExitCode())
+		}
+		fmt.Println("Err: %v", err)
 	}
 }
