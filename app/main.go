04d0c8de9dc295250d2aafa6e5038b834aade1c1
murray <murray.watts@lastyard.com>
2023-04-30 08:09:59 +0800

    refactor

--- app/main.go
+++ app/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"path"
 	"syscall"
 )
 
@@ -16,12 +18,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	err = os.MkdirAll(chrootDir+"/usr/local/bin", 0755)
 	err = os.MkdirAll(chrootDir+"/dev/null", 0755)
-	err = exec.Command("cp", "/usr/local/bin/docker-explorer", chrootDir+"/usr/local/bin").Run()
+	if err != nil {
+		fmt.Println("Err:", err)
+		os.Exit(1)
+	}
 
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
+
+	err = copyExecutableIntoDir(command, chrootDir)
+	if err != nil {
+		fmt.Println("Err:", err)
+		os.Exit(1)
+	}
 
 	// Change root directory to chroot directory
 	if err := syscall.Chroot(chrootDir); err != nil {
@@ -51,4 +61,37 @@ func main() {
 		}
 		fmt.Println("Err:", err)
 	}
+}
+
+// eg. /usr/lib/docker-explorerer -> chroot/usr/lib/docker-explorerer
+func copyExecutableIntoDir(chrootDir string, executablePath string) error {
+	executablePathInChrootDir := path.Join(chrootDir, executablePath)
+
+	if err := os.MkdirAll(path.Dir(executablePathInChrootDir), 0750); err != nil {
+		return err
+	}
+
+	return copyFile(executablePath, executablePathInChrootDir)
+}
+
+func copyFile(sourceFilePath, destinationFilePath string) error {
+	sourceFileStat, err := os.Stat(sourceFilePath)
+	if err != nil {
+		return err
+	}
+
+	sourceFile, err := os.Open(sourceFilePath)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destinationFile, err := os.OpenFile(destinationFilePath, os.O_RDWR|os.O_CREATE, sourceFileStat.Mode())
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+
+	_, err = io.Copy(destinationFile, sourceFile)
+	return err
 }
